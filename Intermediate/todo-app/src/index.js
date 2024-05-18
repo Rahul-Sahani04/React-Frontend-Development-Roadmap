e3f863175a1a2bfcd1e8a657f536fb580da3f564
Rahul <110347707+Rahul-Sahani04@users.noreply.github.com>
2024-05-18 16:42:09 +0530

    Update React rendering method in index.js to use ReactDOM.createRoot for improved performance

--- Intermediate/todo-app/src/index.js
+++ Intermediate/todo-app/src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
